5ae2004409004119ac394965785704c0430797aa
Moises-Rander <105607031+Moises-Rander@users.noreply.github.com>
2022-05-15 17:49:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
 from person import Person
 
 people = []
+registered_cpfs = []
+while True:
+    list_length = input('Quantas pessoas serão registradas? ')
+    if list_length.isnumeric():
+        list_length = int(list_length)
+        break
+    else:
+        print('Por favor, digite um número.')
 
 
 def cpfvalidation(cpf):
-    if len(cpf) == 11:
-        validador = True
-        for i in range(len(cpf)):
-            if cpf[i] not in '0123456789':
-                validador = False
-        return validador
+    if cpf not in registered_cpfs:
+        if len(cpf) == 11:
+            validador = True
+            for i in range(len(cpf)):
+                if cpf[i] not in '0123456789':
+                    validador = False
+                    print('Por favor, digite o seu CPF utilizando apenas números.')
+                    break
+            return validador
+        else:
+            print('Por favor, digite o seu CPF de 11 dígitos.')
+            return False
     else:
-        return False
+        print('CPF já registrado. Por favor, informe um novo CPF.')
 
 
 def datainput():
-    for i in range(3):
-        if i != 0:
-            print()
+    for i in range(list_length):
         while True:
-            name_aux = input(f'Informe o nome da {i+1}ª pessoa: ').strip().title()
+            name_aux = input(f'\nInforme o nome da {i+1}ª pessoa: ').strip().capitalize()
             if name_aux.replace(' ', '').isalpha():
                 break
             else:
-                print('Por favor, digite o seu nome utilizando apenas letras.\n')
+                print('Por favor, digite o seu nome utilizando apenas letras.')
         while True:
-            age_aux = input(f'Informe a idade da {i + 1}ª pessoa: ').replace(' ', '')
+            age_aux = input(f'\nInforme a idade da {i + 1}ª pessoa: ').replace(' ', '')
             if age_aux.isnumeric():
                 break
             else:
-                print('Por favor, digite a sua idade utilizando apenas números.\n')
+                print('Por favor, digite a sua idade utilizando apenas números.')
         while True:
-            cpf_aux = input(f'Informe o CPF da {i+1}ª pessoa: ').replace('.', '').replace('-', '').replace(' ', '')
+            cpf_aux = input(f'\nInforme o CPF da {i+1}ª pessoa: ').replace('.', '').replace('-', '').replace(' ', '')
             if cpfvalidation(cpf_aux):
+                registered_cpfs.append(cpf_aux)
                 break
-            else:
-                print('Por favor, digite o seu CPF utilizando apenas números.\n')
         people.append(Person(name_aux, age_aux, cpf_aux))
 
 
